360ed55b5c879e9caa0a895f401b38cfef4d4d64
chenxiaolu <1981194962@qq.com>
2024-03-23 21:59:54 +0800

    前端之前

--- src/main/java/com/bit/controller/SysUserController.java
+++ src/main/java/com/bit/controller/SysUserController.java
@@ -1,20 +1,29 @@
 package com.bit.controller;
 
 import com.bit.config.redis.RedisService;
+import com.bit.entity.Permission;
+import com.bit.entity.User;
+import com.bit.entity.UserInfo;
 import com.bit.utils.JwtUtils;
+import com.bit.utils.MenuTree;
 import com.bit.utils.Result;
+import com.bit.vo.RouterVo;
 import com.bit.vo.TokenVo;
 import io.jsonwebtoken.Jwts;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/SysUser")
@@ -63,4 +72,50 @@ public class SysUserController {
 //        返回数据
         return Result.ok(tokenVo).message("token刷新成功");
     }
+
+//    查询用户信息
+    @GetMapping("/getInfo")
+    public Result getInfo(){
+        //从Spring security上下文中获取用户信息
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        判断身份信息authentication是否为空
+        if (authentication==null){
+            return Result.error().message("用户信息查询失败");
+        }
+//        获取用户信息
+        User user = (User) authentication.getPrincipal();
+//        获取该用户拥有的角色权限信息
+        List<Permission> permissionList = user.getPermissionList();
+//        获取权限编码
+        Object[] roles = permissionList.stream().filter(Objects::nonNull).map(Permission::getCode).toArray();
+//        创建用户信息
+        UserInfo userInfo = new UserInfo(user.getId(),user.getNickName(),user.getAvatar(),null,roles);
+//        返回数据
+        return Result.ok(userInfo).message("用户信息查询成功");
+    }
+
+    /**
+     * 获取登录用户的菜单数据
+     * @return
+     */
+    @GetMapping("/getMenuList")
+    public Result getMenuList(){
+        //从Spring security上下文中获取用户信息
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        //        获取用户信息
+        User user = (User) authentication.getPrincipal();
+//        获取该用户拥有的角色权限信息
+        List<Permission> permissionList = user.getPermissionList();
+        //筛选当前用户拥有的目录和菜单数据
+        List<Permission> collect = permissionList.stream()
+                //只筛选目录和菜单数据，按钮不需要添加到路由菜单中
+                .filter(item -> item != null && item.getType() != 2) //0-目录 1-菜单 2-按钮
+                .collect(Collectors.toList());
+        // 生成路由数据
+        List<RouterVo> routerVoList = MenuTree.makeRouter(collect, 0L);
+        //返回数据
+        return Result.ok(routerVoList).message("菜单数据获取成功");
+
+    }
 }
+
